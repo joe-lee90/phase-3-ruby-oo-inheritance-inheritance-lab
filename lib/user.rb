7961c3b131c5d958cb5d7d2f66a3787336006c13
Joe <zvmboni@gmail.com>
2022-05-07 20:36:54 -0700

    commit tree 3fbda4d997b2cd0980c15ab6d2f7530cced82731

---
+++ lib/user.rb
@@ -0,0 +1,22 @@
+class User
+    def initialize()
+        @first_name
+        @last_name
+    end
+
+    def first_name=(firstName)
+        @first_name = firstName
+    end
+
+    def last_name=(lastName)
+        @last_name = lastName
+    end
+
+    def first_name
+        @first_name
+    end
+
+    def last_name
+        @last_name
+    end
+end
